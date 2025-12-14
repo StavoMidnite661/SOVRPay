f47e7c2339eb5cf158b8b43aef19e80561e5139d
stavogm <stavogm@gmail.com>
2025-12-14 01:06:39 +0000

    refactor: Remove unnecessary files and assets

--- attached_assets/bank_1756546694630.ts
+++
@@ -1,8 +0,0 @@
-import fs from "fs";
-import path from "path";
-export async function submitLocal(nachaText: string, tag: string) {
-  const p = path.join(process.cwd(), `payroll_${tag}.ach`);
-  fs.writeFileSync(p, nachaText, "utf8");
-  return { mode: "LOCAL", path: p };
-}
-// TODO: add SFTP and gateway API implementations
